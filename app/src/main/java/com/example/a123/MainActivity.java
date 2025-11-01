d3e46b18fa9a780230b01635ee88938dcced9cdc
AlyonaUzhakova <alyonauzhakova@gmail.com>
2025-11-01 11:49:40 +0500

    Разработанные страницы

--- app/src/main/java/com/example/a123/MainActivity.java
+++ app/src/main/java/com/example/a123/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a123;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-            return insets;
+        setContentView(R.layout.login);
+
         };
+    public void Reg_Text(View view)
+    {
+        setContentView(R.layout.regin);
     }
+}
+
