03a6eff920d155f7b08761d5472a06596abf93bc
Anna <eltyshevaanna2006@yandex.ru>
2026-02-07 15:07:45 +0500

    создан IAuditService для логов

---
+++ RestAPP/Interfaces/IAuditService.cs
@@ -0,0 +1,17 @@
+﻿using RestAPP.Model;
+
+namespace RestAPP.Interfaces
+{
+    public interface IAuditService
+    {
+        Task LogAsync(int actorId, string actorType, string action, string entity, string? details = null);
+
+        Task<List<ActivityLog>> GetAllLogsAsync();
+
+        Task<List<ActivityLog>> GetLogsByActorAsync(int actorId);
+
+        Task ClearAllAsync();
+
+        Task TrimAsync(int keepLastCount = 1000);
+    }
+}
