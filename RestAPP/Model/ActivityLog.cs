91471adbce7f4977d073f3e40a451f37b1a2bb71
Anna <eltyshevaanna2006@yandex.ru>
2026-02-07 15:08:24 +0500

    создан ActivityLog для логов

---
+++ RestAPP/Model/ActivityLog.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestAPP.Model
+{
+    public class ActivityLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ActorId { get; set; }                // ID пользователя или администратора
+        [Required, StringLength(50)]
+        public string ActorType { get; set; }           // "User" или "Admin"
+
+        [Required, StringLength(50)]
+        public string Action { get; set; }              // LOGIN, REGISTER, CREATE, UPDATE, DELETE, BOOKING и т.д.
+
+        [Required, StringLength(50)]
+        public string Entity { get; set; }              // User, Booking, Restaurant, Dish, RestaurantDish, Auth и т.д.
+
+        [StringLength(1000)]
+        public string? Details { get; set; }            // дополнительная информация
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
