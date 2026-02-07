fa68f0cc6c3c55c8f2759ea21a80e27944685432
Tashikv <vozhakovatasha1@gmail.com>
2026-02-07 15:21:55 +0500

    Реализован NewsContext

--- RestAPP/Context/NewsContext.cs
+++ RestAPP/Context/NewsContext.cs
@@ -1,6 +1,22 @@
-﻿namespace RestAPP.Context
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPI.Context;
+using RestAPI.Model;
+using RestAPI.Models;
+
+namespace RestAPP.Context
 {
-    public class NewsContext
+    public class NewsContext : DbContext
     {
+        public DbSet<News> News { get; set; }
+
+        public NewsContext(DbContextOptions<NewsContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Эта строка гарантирует, что EF пойдет в таблицу "News" в схеме "dbo"
+            modelBuilder.Entity<News>().ToTable("News", "dbo");
+        }
     }
 }
