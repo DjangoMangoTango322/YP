bd5397e02678c87b1fbed0c07947ca9c6a3c2f49
Anna <eltyshevaanna2006@yandex.ru>
2026-02-07 15:19:18 +0500

    Создана разметка для логов в десктоп

--- Desktop/Models/Log.cs
+++ Desktop/Models/Log.cs
@@ -9,11 +9,11 @@ namespace Desktop.Models
     public class Log
     {
         public int Id { get; set; }
-        public int ActorId { get; set; }           // ID пользователя или админа
-        public string ActorType { get; set; }      // "User" или "Admin"
-        public string Action { get; set; }         // LOGIN, CREATE, UPDATE и т.д.
-        public string Entity { get; set; }         // User, Booking, Restaurant...
-        public string Details { get; set; }        // дополнительные данные
+        public int ActorId { get; set; }          
+        public string ActorType { get; set; }    
+        public string Action { get; set; }       
+        public string Entity { get; set; }       
+        public string Details { get; set; }   
         public DateTime Timestamp { get; set; }
     }
 }
