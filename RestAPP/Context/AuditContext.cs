5f7e94b1cc549ccf8c1ad813f6f29f3ba06b7c18
Anna <eltyshevaanna2006@yandex.ru>
2026-02-07 15:02:00 +0500

    добавлен класс AuditContext для логов

---
+++ RestAPP/Context/AuditContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPP.Model;
+
+
+namespace RestAPP.Context
+{
+    public class AuditContext
+    {
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+
+        public AuditContext(DbContextOptions<AuditContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
