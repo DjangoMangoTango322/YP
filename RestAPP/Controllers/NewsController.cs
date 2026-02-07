f6ea1e0f20bf927b89454c36bd61d1120c9c9be9
Tashikv <vozhakovatasha1@gmail.com>
2026-02-07 15:26:23 +0500

    Реализован контроллер NewsController

--- RestAPP/Controllers/NewsController.cs
+++ RestAPP/Controllers/NewsController.cs
@@ -1,6 +1,40 @@
-﻿namespace RestAPP.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using RestAPI.Interfaces;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
 {
-    public class NewsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
     {
+        private readonly INewsService _newsService;
+
+        public NewsController(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [HttpGet("GetAllNews")]
+        public async Task<IActionResult> GetAllNews()
+        {
+            var news = await _newsService.GetAllNews();
+            return Ok(news);
+        }
+
+        [HttpPost("RefreshNews")]
+        public async Task<IActionResult> RefreshNews()
+        {
+            await _newsService.ParseNewsFromSource();
+            return Ok("База обновлена");
+        }
+
+        [HttpDelete("DeleteNews/{id}")]
+        public async Task<IActionResult> DeleteNews(int id)
+        {
+            // Правильный вызов:
+            await _newsService.DeleteNews(id);
+            return Ok("Запись удалена");
+        }
     }
 }
