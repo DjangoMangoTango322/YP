6284ddc6cc63fa51dbf4fca284d07fa0de9f3dfc
Tashikv <vozhakovatasha1@gmail.com>
2026-02-07 18:18:18 +0500

    добавлен Achievement для системы достижений

--- RestAPP/Model/Achievement.cs
+++ RestAPP/Model/Achievement.cs
@@ -8,6 +8,6 @@ namespace RestAPI.Model
         [Required]
         public string Name { get; set; } // "Новичок" и т.д.
         public string Description { get; set; }
-        public int Threshold { get; set; } // Количество бронирований для получения (1, 5, 10, 15)
+        public int Threshold { get; set; } 
     }
 }
